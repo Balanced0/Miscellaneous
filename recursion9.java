2f412bbe237171a0206efbbdfd35843b9dc2c7c0
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-08-02 21:38:06 +0600

    Sorted Array

---
+++ recursion9.java
@@ -0,0 +1,14 @@
+public class recursion9{
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        boolean ans = sort(arr, 0);
+        System.out.println(ans);
+    }
+    
+    public static boolean sort(int[] arr, int index){
+        if(index == arr.length - 1){
+            return true;
+        }
+        return arr[index] < arr[index + 1] && sort(arr, index + 1);
+    }
+}
